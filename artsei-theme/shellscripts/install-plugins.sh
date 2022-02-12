1f2f1e4bdef40a6bea39904d32b0cc9ae3a6873a
ToshiWani <tkunisada@gmail.com>
2022-02-12 13:07:51 -0500

    moved shell scripts

---
+++ artsei-theme/shellscripts/install-plugins.sh
@@ -0,0 +1,4 @@
+wp plugin install wp-multibyte-patch --activate
+wp plugin install advanced-custom-fields --activate
+wp plugin install folders --activate
+wp plugin install all-in-one-wp-migration --activate
--- artsei-theme/shellscripts/update-config.sh
+++ artsei-theme/shellscripts/update-config.sh
@@ -2,7 +2,7 @@ wp option update blogname "art-sei.com" --allow-root
 wp option update blogdescription "生活に密着した生の人間を描きたい。現代画家、千光士誠のサイト"  --allow-root
 wp option update admin_email "tkunisada@gmail.com"  --allow-root
 
-wp theme activate art-sei-com --allow-root
+wp theme activate artsei-theme --allow-root
 
 wp post create --post_title='トップページ' --post_name='front-page' --post_type=page --post_status=publish  --allow-root
 wp post create --post_title='作品一覧' --post_name='portfolio' --post_type=page --post_status=publish  --allow-root
