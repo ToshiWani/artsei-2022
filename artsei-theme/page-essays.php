a412b0c292d991d351d36ca0dce8c624cf41466f
ToshiWani <tkunisada@gmail.com>
2022-02-13 14:54:29 -0500

    categolized essays by year

--- artsei-theme/page-essays.php
+++ artsei-theme/page-essays.php
@@ -26,32 +26,45 @@ get_header();?>
 
         <?php 
             global $wpdb;
-            $query = "select ID as post_id, p.post_title
+            $query = "select p.ID as post_id, p.post_title, year(p.post_date) as post_year
             from wp_posts p 
             where p.`post_type` = 'post' and p.`post_status`='publish'
             order by p.post_date_gmt desc";
 
             $results = $wpdb->get_results($query);
-            $post_rows = null;
-            if($results) {
-                $post_rows = array_chunk($results, 4);
+            $post_list = [];
+
+            foreach($results as $item) {
+                $post_obj = new stdClass();
+                $post_obj->post_id = $item->post_id;
+                $post_obj->post_title = $item->post_title;
+                $post_obj->url = get_permalink($item->post_id);
+
+                $post_list[$item->post_year][] = $post_obj;
             }
         ?>
 
-        <?php if ($post_rows): ?>
-            <?php foreach($post_rows as $row): ?>
-                <div class="row aln-center">
-                    <?php foreach($row as $col): ?>
-                        <div class="col-3 col-6-small col-12-xsmall" style="padding: 0.512rem 0.64rem 0.512rem;">
-                            <a class="button fit" style="cursor: pointer;" href="<?php echo get_permalink($col->post_id); ?>"><?php echo $col->post_title ?></a>
+        <?php if ($post_list) : ?>
+            <?php foreach($post_list as $year => $posts_by_year) : ?>
+                <h2><?php echo $year; ?>年</h2>
+                <?php $row_list = array_chunk($posts_by_year, 4); ?>
+                <?php foreach ($row_list as $row) : ?>
+                    <?php $row = array_pad($row, 4, false); ?>
+                    <div class="row aln-center">
+                    <?php foreach($row as $post) : ?>
+                        <div class="col-3 col-6-small col-12-xsmall" style="margin-top: 0.512rem; ">
+                            <?php if($post) : ?>
+                            <a class="button fit" style="cursor: pointer; font-size:0.96rem;" href="<?php echo $post->url; ?>">
+                                <?php echo $post->post_title ?>
+                            </a>
+                            <?php endif; ?>
                         </div>
-                    <?php endforeach; ?>
-                </div>
-            <?php endforeach; ?>
+                    <?php endforeach; /* end column */ ?>
+                    </div>
+                <?php endforeach; /* end row */ ?>
+                <hr />
+            <?php endforeach; /* end year */ ?>
         <?php endif; ?>
-
-        <?php //the_content(); ?>
-        
     </div>
 </section>
 
